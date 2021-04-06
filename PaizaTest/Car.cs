a1db44deab7fac20a613c344a50680db33207268
HachiChan <yatahachi@gmail.com>
2021-04-07 06:38:47 +0900

    Initial commit

---
+++ PaizaTest/Car.cs
@@ -0,0 +1,48 @@
+﻿namespace PaizaTest
+{
+    public class Car
+    {
+        // メンバ変数
+        // アクセス修飾子がprivateのため、このclass内からしか参照できない(このclass内からしか変更されないことを保証できる)
+        // privateの場合は省略可能
+        string _maker = string.Empty;
+        private string _model = string.Empty;
+        private double _fuel = -1;
+        private double _consumption = -1;
+
+        // コンストラクタ
+        // 自身を"インスタンス化(newステートメントを使用)"した際に自動で一度だけ呼ばれるメソッド
+        // コンストラクタもメソッドのため、引数がある場合インスタンス化時に指定しないとエラー
+        public Car(string maker, string model, double fuel, double consumption)
+        {
+            _maker = maker;
+            _model = model;
+            _fuel = fuel;
+            _consumption = consumption;
+        }
+        
+        // 以下メソッド
+        // 自身のメンバ変数を参照して返す（メンバ変数はprivateだが同じclass内なので参照できる）
+        
+        public string Maker()
+        {
+            // 自身を指す "this" は省略可能
+            return this._maker;
+        }
+
+        public string Model()
+        {
+            return _model;
+        }
+
+        public double Fuel()
+        {
+            return _fuel;
+        }
+
+        public double Consumption()
+        {
+            return _consumption;
+        }
+    }
+}
---
+++ PaizaTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PaizaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Car型の変数を宣言してCar型をインスタンス化（Car型という設計図から実態(インスタンス)を作り出すイメージ）
+            // Car型はコンストラクタで引数指定があるので必須
+            Car prius = new Car("トヨタ", "プリウス", 40, 30.5);
+            
+            // 出力
+            Console.WriteLine("出力結果:");
+            Console.WriteLine($"メーカー: {prius.Maker()}, 車種: {prius.Model()}, 燃料: {prius.Fuel()}, 燃費: {prius.Consumption()}");
+            
+            // これはコンパイルが通らない
+            // priusインスタンスの外(ここはProgramクラス)からprivateな変数を参照しているため
+            Console.WriteLine(prius._maker);
+            
+            // prius._makerを参照したい場合は、publicかつ_makerを返すprius.Maker()メソッドをよびだす
+            Console.WriteLine(prius.Maker());
+        }
+
+        // privateは省略できる
+        void SampleMethod()
+        {
+            // staticについて
+            // staticなメソッドはインスタンスからでなくとも呼び出せる(newしなくてもいい。プログラムの初期化時にメモリに乗り、最後まで消えない)
+            var result1 = Program.SampleStaticMethod();
+            
+            // staticじゃない場合はインスタンスからじゃないと呼べない
+            // ↓エラー
+            var result2 = Program.SampleInstanceMethod();
+            
+            // ↓動く
+            var program = new Program();
+            var result3 = program.SampleInstanceMethod();
+        }
+
+
+        string SampleInstanceMethod()
+        {
+            return "staticじゃないよ";
+        }
+        
+        static string SampleStaticMethod()
+        {
+            return "staticだよ";
+        }
+        
+    }
+}
